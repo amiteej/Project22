229c2aa06e7fbb6c878df7c29fb39b3590bfdf75
Amiteej Munagala <amiteejm@gmail.com>
2021-01-12 16:38:19 +0530

    final commit

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 var starImg, fairyImg, bgImg;
-var fairy , fairyVoice;
+var fairy, fairyVoice;
 var star, starBody;
 
 const Engine = Matter.Engine;
@@ -7,10 +7,9 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-function preload()
-{
+function preload() {
 	starImg = loadImage("images/star.png");
-	fairyImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
+	fairyImg = loadAnimation("images/fairyImage1.png", "images/fairyImage2.png");
 	bgImg = loadImage("images/starNight.png");
 	fairyVoice = loadSound("sound/JoyMusic.mp3");
 
@@ -19,34 +18,53 @@ function preload()
 function setup() {
 	createCanvas(800, 750);
 
-	// fairyVoice.play();
+	fairyVoice.play();
 
 	fairy = createSprite(130, 520);
-	fairy.addAnimation("fairyflying",fairyImg);  
-	fairy.scale =0.25;
+	fairy.addAnimation("fairyflying", fairyImg);
+	fairy.scale = 0.25;
 
-	star = createSprite(650,30);
+	var options = {
+		"restitution": 0.5,
+		"isStatic": true
+	}
+
+	star = createSprite(650, 30);
 	star.addImage(starImg);
 	star.scale = 0.2;
 
 	engine = Engine.create();
 	world = engine.world;
 
-	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
+	starBody = Bodies.circle(650, 30, 5, options);
 	World.add(world, starBody);
-	
-	Engine.run(engine);
 
+	Engine.run(engine);
 }
 
 
 function draw() {
-  background(bgImg);
+	background(bgImg);
+	star.x = starBody.position.x;
+	star.y = starBody.position.y;
 
-  drawSprites();
+	if(star.y>470 && startBody.position.y>470){
+		Matter.Body.setStatic(starBody,true);
+	}
 
+	drawSprites();
 }
 
 function keyPressed() {
 	//write code here
+	if (keyCode === RIGHT_ARROW) {
+		fairy.velocityX = fairy.velocityX +5;
+	}
+
+	if (keyCode === LEFT_ARROW) {
+		fairy.velocityX = fairy.velocityX - 5;
+	}
+	if (keyCode === DOWN_ARROW) {
+		Matter.Body.setStatic(starBody,false);
+	}
 }
